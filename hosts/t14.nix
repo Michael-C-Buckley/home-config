2a7320fe2aff4943bb91f0abbe534bf03aca4639
Michael Buckley <michaelcbuckley@proton.me>
2025-01-09 21:34:02 -0500

    Corrected state version reference

--- hosts/t14.nix
+++ hosts/t14.nix
@@ -4,5 +4,5 @@
     ../modules/hyprland/hosts/t14.nix
     ./extras.nix
   ];
-  home-manager.users.michael.home.stateVersion = "24.05";
+  home.stateVersion = "24.05";
 }
--- hosts/x570.nix
+++ hosts/x570.nix
@@ -4,5 +4,5 @@
     ../modules/hyprland/hosts/x570.nix
     ./extras.nix
   ];
-  home-manager.users.michael.home.stateVersion = "24.05";
+  home.stateVersion = "24.05";
 }
