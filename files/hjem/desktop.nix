62e33cea7bba0a20916842bbd4e1da207f1d1f00
Michael Buckley <michaelcbuckley@proton.me>
2025-01-18 15:57:30 -0500

    Fixed project structure and imports

--- files/hjem/desktop.nix
+++
@@ -1,6 +0,0 @@
-# Adds configs for GUI and other non-Server functionality
-{...}: {
-  hjem.users.michael.files = {
-    "ghostty/config".source = ./userfiles/ghostty;
-  };
-}
--- hosts/x570.nix
+++ hosts/x570.nix
@@ -3,6 +3,7 @@
     ../modules/hyperland
     ../modules/hyprland/hosts/x570.nix
     ../modules/graphics.nix
+    ../files/desktop.nix
     ./extras.nix
   ];
   home.stateVersion = "24.05";
