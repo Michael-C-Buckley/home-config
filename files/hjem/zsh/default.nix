d1b2bb24fe51ed23b24e6672d693744dafd89542
Michael Buckley <michaelcbuckley@proton.me>
2025-01-18 11:49:01 -0500

    Geometry in zsh now uses the flaked packaged version

--- files/hjem/zsh/default.nix
+++ files/hjem/zsh/default.nix
@@ -1,14 +1,8 @@
-{pkgs, ...}: let
-  geometry-zsh = pkgs.fetchFromGitHub {
-    owner = "geometry-zsh";
-    repo = "geometry";
-    rev = "7e245c627d0b4c58dffc59899c9ef9f5027adba0";
-    sha256 = "sha256-kUxtqKQb0OugDkflPQ49OoH51kwgjBHBeNrS1fE8inE=";
-  };
-in {
+{inputs, system, ...}:
+{
   hjem.users.michael = {
     files = {
-      ".config/resources/geometry".source = "${geometry-zsh}";
+      ".config/resources/geometry".source = "${inputs.geometry.packages.${system}}";
       ".zshrc".source = ./default.zshrc;
     };
   };
