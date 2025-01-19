9fe9e1925f5776b012b117f4416d91a701bf1cf4
Michael Buckley <michaelcbuckley@proton.me>
2025-01-18 21:57:23 -0500

    Removed out of store symlinks for now

--- files/zsh/default.nix
+++ files/zsh/default.nix
@@ -1,7 +1,7 @@
-{ lib,... }:
+{ ... }:
 {
   home.file = {
-    ".zshrc".source = lib.file.mkOutOfStoreSymlink ./default.zshrc;
-    ".config/zsh/geometry/myGeometry.zsh".source = lib.file.mkOutOfStoreSymlink ./myGeometry.zsh;
+    ".zshrc".source = ./default.zshrc;
+    ".config/zsh/geometry/myGeometry.zsh".source = ./myGeometry.zsh;
   };
 }
