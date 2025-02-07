3d0b669d95c178b0780c2a8cb6d2fb4a71f4576d
Michael Buckley <michaelcbuckley@proton.me>
2025-02-06 19:50:50 -0500

    Changed broken border to a green one

--- modules/hyprland/lookfeel.nix
+++ modules/hyprland/lookfeel.nix
@@ -8,7 +8,7 @@
     layout = "dwindle";
 
     # https://wiki.hyprland.org/Configuring/Variables/#variable-types for info about colors
-    "col.active_border" = "rgba(130, 198, 105, 0.8) rgba(101, 237, 51, 0.8) 45deg";
+    "col.active_border" = "rgba(00b6ffc9)";
     "col.inactive_border" = "rgba(595959aa)";
 
     # Set to true enable resizing windows by clicking and dragging on borders and gaps
