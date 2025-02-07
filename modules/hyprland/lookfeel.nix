2d6e4bc68c0a9e7f669369f1507d6d5301e1d862
Michael Buckley <michaelcbuckley@proton.me>
2025-02-06 19:56:51 -0500

    Changed the green to be much more subtle and less distracting

--- modules/hyprland/lookfeel.nix
+++ modules/hyprland/lookfeel.nix
@@ -8,7 +8,7 @@
     layout = "dwindle";
 
     # https://wiki.hyprland.org/Configuring/Variables/#variable-types for info about colors
-    "col.active_border" = "rgba(00b6ffc9)";
+    "col.active_border" = "rgba(325e70ab)";
     "col.inactive_border" = "rgba(595959aa)";
 
     # Set to true enable resizing windows by clicking and dragging on borders and gaps
