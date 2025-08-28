98841091ea711a3e282671fce95c7b1103a40709
Michael Buckley <michaelcbuckley@proton.me>
2025-08-28 14:04:21 -0400

    Hjem configs now ships my NVF configs

--- flake.nix
+++ flake.nix
@@ -46,9 +46,13 @@
               ./flake/hjem/default.nix
               ./flake/hjem/extended.nix
             ];
+            # I am currently only using Hjem on x86_64-linux
+            users.users.michael.packages = [inputs.nvf-flake.packages.x86_64-linux.default];
           };
           minimal = _: {
+            # I am currently only using Hjem on x86_64-linux
             imports = [./flake/hjem/default.nix];
+            users.users.michael.packages = [inputs.nvf-flake.packages.x86_64-linux.minimal];
           };
         };
 
