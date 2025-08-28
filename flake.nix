fa723494163ec2041f35d154544b1bddc78147cd
Michael Buckley <michaelcbuckley@proton.me>
2025-08-28 13:53:49 -0400

    Added my NVF to inputs

--- flake.nix
+++ flake.nix
@@ -3,8 +3,19 @@
 
   inputs = {
     nixpkgs.url = "https://channels.nixos.org/nixos-unstable/nixexprs.tar.xz";
+
+    flake-compat.url = "github:edolstra/flake-compat";
     flake-parts.url = "github:/hercules-ci/flake-parts";
     systems.url = "github:/nix-systems/default";
+
+    nvf-flake = {
+      url = "github:Michael-C-Buckley/nvf-flake";
+      inputs = {
+        nixpkgs.follows = "nixpkgs";
+        flake-parts.follows = "flake-parts";
+        systems.follows = "systems";
+      };
+    };
 
     home-manager = {
       url = "github:nix-community/home-manager";
