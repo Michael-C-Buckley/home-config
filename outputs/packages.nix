ca9162ce3817148088044b6296543d6d302d9775
Michael Buckley <michaelcbuckley@proton.me>
2025-09-05 21:09:31 -0400

    Fixed packages namespaces

--- outputs/packages.nix
+++ outputs/packages.nix
@@ -1,14 +1,16 @@
-{inputs, ...}: let
+{inputs, ...}: {
+  perSystem = {pkgs, ...}: let
   mkNvf = extraModules:
     (inputs.nvf.lib.neovimConfiguration {
       inherit pkgs;
       modules = [../flake/packages/nvf] ++ extraModules;
     }).neovim;
 in {
-  perSystem = {pkgs, ...}: {
-    ns = pkgs.callPackage ../flake/packages/ns.nix {};
+  packages = {
+      ns = pkgs.callPackage ../flake/packages/ns.nix {};
 
-    nvf = mkNvf [../flake/packages/nvf/extended.nix];
-    nvf-minimal = mkNvf [];
+      nvf = mkNvf [../flake/packages/nvf/extended.nix];
+      nvf-minimal = mkNvf [];
+    };
   };
 }
